f0c15babfcb49e8eb0c9a012ed877415189c933f
Amira Bousaada <abousaada@augustareeves.fr>
2025-12-10 13:37:42 +0000

    Mission : add mission types

--- webapp/model/utilities/initialData.js
+++ webapp/model/utilities/initialData.js
@@ -1,55 +1,61 @@
-sap.ui.define([], function() {
-    "use strict";
-      return {
-        "year": null,
-        "businessNo": null,
-        "period": null,
-        "missions": [],
-        "recaps": [],
-        "previsions": [],
-        "opport": [],
-        "risque": [],
-        "charts": [],
-        "chartsAdditionalData": [],
-        "pxAutres": [],
-        "pxSousTraitance":[],
-        "pxRecetteExt":[],
-        "PxAutreHierarchyWithTotals": [],
-        "pxRecetteExtHierarchy": [],
-        "pxSubContractingHierarchy": [],
-        "pxSubContractingHeader": [
-          /*{
-            subContractorId       : "SubContractor",
-            subContractorName     : "SubContractorName",
-            subContractorPartner  : "SubContractorPartner",
-          }*/
-        ],
-        "pxMainOeuvre": [],
-        "pxMainOeuvreHierarchy": [],
-        "pxMainOeuvreHeader": [
-          /*{
-            subContractorId       : "SubContractor",
-            subContractorName     : "SubContractorName",
-            subContractorPartner  : "SubContractorPartner",
-          }*/
-        ],
-        "pxSTI": [],
-        "pSTI": [],
-        "Notes": '',
-        "sfgp": [],
-        "previsionel": [],
-        "missionTypes": [
-            { code: "GEN", description: "GEN" },
-            { code: "AVP", description: "AVP" },
-            { code: "PRO", description: "PRO" },
-            { code: "ACT", description: "ACT" },
-            { code: "DCE", description: "DCE" },
-            { code: "EXE", description: "EXE" }
-        ],
-        "missionStatus": [
-          { code: "A", description: "Acquis" },
-          { code: "N", description: "Non Acquis" },
-          { code: "R", description: "Réclamé" }
-        ]
-      };
-  });
+sap.ui.define([], function () {
+  "use strict";
+  return {
+    "year": null,
+    "businessNo": null,
+    "period": null,
+    "missions": [],
+    "recaps": [],
+    "previsions": [],
+    "opport": [],
+    "risque": [],
+    "charts": [],
+    "chartsAdditionalData": [],
+    "pxAutres": [],
+    "pxSousTraitance": [],
+    "pxRecetteExt": [],
+    "PxAutreHierarchyWithTotals": [],
+    "pxRecetteExtHierarchy": [],
+    "pxSubContractingHierarchy": [],
+    "pxSubContractingHeader": [
+      /*{
+        subContractorId       : "SubContractor",
+        subContractorName     : "SubContractorName",
+        subContractorPartner  : "SubContractorPartner",
+      }*/
+    ],
+    "pxMainOeuvre": [],
+    "pxMainOeuvreHierarchy": [],
+    "pxMainOeuvreHeader": [
+      /*{
+        subContractorId       : "SubContractor",
+        subContractorName     : "SubContractorName",
+        subContractorPartner  : "SubContractorPartner",
+      }*/
+    ],
+    "pxSTI": [],
+    "pSTI": [],
+    "Notes": '',
+    "sfgp": [],
+    "previsionel": [],
+    "missionTypes": [
+      { code: "ESQ", description: "ESQ" },
+      { code: "AVP", description: "AVP" },
+      { code: "APS", description: "APS" },
+      { code: "APD", description: "APD" },
+      { code: "PRO", description: "PRO" },
+      { code: "ACT", description: "ACT" },
+      { code: "EXE", description: "EXE" },
+      { code: "DET", description: "DET" },
+      { code: "OPC", description: "OPC" },
+      { code: "AOR", description: "AOR" },
+      { code: "DIA", description: "DIA" }
+    ]
+    ,
+    "missionStatus": [
+      { code: "A", description: "Acquis" },
+      { code: "N", description: "Non Acquis" },
+      { code: "R", description: "Réclamé" }
+    ]
+  };
+});
